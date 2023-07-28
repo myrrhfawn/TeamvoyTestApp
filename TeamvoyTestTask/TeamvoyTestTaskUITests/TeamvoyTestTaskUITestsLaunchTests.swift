b633e43ba971bde7ecccef794e2c2ec24ab43463
Ростик <rostyk@Mac-Rostik.local>
2023-07-29 01:50:05 +0300

    create git repository

---
+++ TeamvoyTestTask/TeamvoyTestTaskUITests/TeamvoyTestTaskUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TeamvoyTestTaskUITestsLaunchTests.swift
+//  TeamvoyTestTaskUITests
+//
+//  Created by Ростик on 29.07.2023.
+//
+
+import XCTest
+
+final class TeamvoyTestTaskUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
