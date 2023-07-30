f050868a0de09cc85c3a9287701a70ef8249efb2
Ростик <rostyk@Mac-Rostik.local>
2023-07-30 07:47:24 +0300

    added search by word

--- TeamvoyTestTask/TeamvoyTestTask/ViewController.swift
+++ TeamvoyTestTask/TeamvoyTestTask/ViewController.swift
@@ -11,12 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     // MARK: -IBOutlets
     @IBOutlet private var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
     private var articles: [Article] = []
+        
+    var filteredData = [Article]()
+    var isSearching = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.searchBar.delegate = self
         tableView.dataSource = self
         let nib = UINib(nibName: "ArticleCell", bundle: nil)
         tableView.register(nib, forCellReuseIdentifier: "ArticleCell")
@@ -37,13 +42,48 @@ class ViewController: UIViewController {
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        articles.count
+        if isSearching {
+            return filteredData.count
+        } else {
+            return articles.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleCell", for: indexPath) as! ArticleCell
-        cell.configure(with: articles[indexPath.row].title, description: articles[indexPath.row].description)
+       
+        if isSearching {
+            cell.configure(with: filteredData[indexPath.row].title, description: filteredData[indexPath.row].description)
+        } else {
+            cell.configure(with: articles[indexPath.row].title, description: articles[indexPath.row].description)
+        }
+            
         return cell
     }
 }
 
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        self.filteredData.removeAll()
+        
+        if searchBar.text == "" {
+            isSearching = false
+            tableView.reloadData()
+        } else {
+            isSearching = true
+            for item in articles {
+                let text = searchText.lowercased()
+                let isArrayContain = item.title.lowercased().range(of: text)
+                if isArrayContain != nil {
+                    print("Search complete")
+                    filteredData.append(item)
+                }
+            }
+            if filteredData.count == 0 {
+                tableView.backgroundView = nil
+            }
+            tableView.reloadData()
+        }
+    }
+}
